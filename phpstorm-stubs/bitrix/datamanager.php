2e3492c36c9b897b3da35cb7836f97330ea414c9
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2020-03-03 16:54:19 +0300

    phpdtorm-stubs

---
+++ phpstorm-stubs/bitrix/datamanager.php
@@ -0,0 +1,9 @@
+<?php
+
+// https://github.com/VKCOM/noverify/issues/361
+
+namespace Bitrix\Main\Entity;
+
+class DataManager extends \Bitrix\Main\ORM\Data\DataManager
+{
+}
---
+++ phpstorm-stubs/bitrix/init.php
@@ -0,0 +1,9 @@
+<?php
+
+define('B_PROLOG_INCLUDED', true);
+
+//global $APPLICATION;
+//global $DB;
+//global $USER;
+
+//global $arParams;
